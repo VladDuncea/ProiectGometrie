e00aaa228bfd5804537dcddcf9d3b3afe7452540
vlad_stoleru <vlad_stoleru@yahoo.com>
2019-12-14 23:55:04 +0200

    .

--- TemaGeometrie/main.cpp
+++ TemaGeometrie/main.cpp
@@ -15,14 +15,10 @@ int main() {
 		Polygon.push_back(newPoint);
 	}
 	fin.close();
-	if (orientation(Polygon[n - 1], Polygon[0], Polygon[1]) < 0)
-		Concaves.push_back(Polygon[0]);
-	for (size_t i = 1; i < n - 1; ++i) {
-		if (orientation(Polygon[i - 1], Polygon[i], Polygon[i + 1]) < 0)
+	for (size_t i = 0; i < n; ++i) {
+		if (orientation(Polygon[(i - 1) % n], Polygon[i], Polygon[(i + 1) % n]) < 0)
 			Concaves.push_back(Polygon[i]);
 	}
-	if (orientation(Polygon[n - 2], Polygon[n - 1], Polygon[0]) < 0)
-		Concaves.push_back(Polygon[n - 1]);
 	for (auto point : Concaves)
 		cout << point << ' ';
 	Polygon.clear();
