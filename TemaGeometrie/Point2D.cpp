058e036997e71832c355ec80809ace8673fad7cc
vlad_stoleru <vlad_stoleru@yahoo.com>
2019-12-14 23:52:21 +0200

    Determined concave angles

--- TemaGeometrie/Point2D.cpp
+++ TemaGeometrie/Point2D.cpp
@@ -19,3 +19,24 @@ ostream& operator<<(ostream& out, const Point2D& Obj) {
 	out << '(' << Obj.X << ", " << Obj.Y << ')';
 	return out;
 }
+
+double orientation(const Point2D& A, const Point2D& B, const Point2D& C) {
+	return (A.X * B.Y + B.X * C.Y + C.X * A.Y) - (A.Y * B.X + B.Y * C.X + C.Y * A.X);
+}
+
+bool isInside(const Point2D& A, const Point2D& B, const Point2D& C, const Point2D& P) {
+	/* Calculate area of triangle ABC */
+	double ABC = abs(orientation(A, B, C));
+
+	// Calculate area of triangle PBC
+	double PBC = abs(orientation(P, B, C));
+
+	// Calculate area of triangle PAC 
+	double APC = abs(orientation(A, P, C));
+
+	// Calculate area of triangle PAB 
+	double ABP = abs(orientation(A, B, P));
+
+	// Check if sum of A1, A2and A3 is same as A
+	return (ABC == PBC + APC + ABP);
+}
--- TemaGeometrie/Point2D.h
+++ TemaGeometrie/Point2D.h
@@ -12,24 +12,7 @@ public:
 	friend ostream& operator<<(ostream& out, const Point2D& Obj);
 };
 
-double orientation(const Point2D& A, const Point2D& B, const Point2D& C) {
-	return abs((A.X * B.Y + B.X * C.Y + C.X * A.Y) - (A.Y * B.X + B.Y * C.X + C.Y * A.X));
-}
+double orientation(const Point2D& A, const Point2D& B, const Point2D& C);
 
-bool isInside(const Point2D& A, const Point2D& B, const Point2D& C, const Point2D& P) {
-	/* Calculate area of triangle ABC */
-	double ABC = abs(orientation(A, B, C));
-
-	// Calculate area of triangle PBC
-	double PBC = abs(orientation(P, B, C));
-
-	// Calculate area of triangle PAC 
-	double APC = abs(orientation(A, P, C));
-
-	// Calculate area of triangle PAB 
-	double ABP = abs(orientation(A, B, P));
-
-	// Check if sum of A1, A2and A3 is same as A
-	return (ABC == PBC + APC + ABP);
-}
+bool isInside(const Point2D& A, const Point2D& B, const Point2D& C, const Point2D& P);
 
